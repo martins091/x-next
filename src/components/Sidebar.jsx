39cc0dc61336a7d9a943664aec1f18071adda440
martins091 <agbomartinsejiofor@gmail.com>
2024-05-11 15:51:53 +0100

    add next auth and modify sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,46 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { FaXTwitter } from "react-icons/fa6";
 import { TiHome } from "react-icons/ti";
 
 export default function Sidebar() {
+  const { data: session } = useSession();
+
   return (
-    <div className='flex flex-col gap-4 p-3'>
+    <div className="flex flex-col gap-4 p-3">
       <Link href="/">
         <FaXTwitter
           className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 
             rounded-full transition-all duration-200"
         />
       </Link>
-      <Link href='/' className="flex items-center p-3 hover:bg-gray-100 
-      rounded-full transition-all duration-200 gap-2 w-fit" >
-        <TiHome 
-            className="w-7 h-7"
-        />
+      <Link
+        href="/"
+        className="flex items-center p-3 hover:bg-gray-100 
+      rounded-full transition-all duration-200 gap-2 w-fit"
+      >
+        <TiHome className="w-7 h-7" />
         <span className="font-bold hidden xl:inline">Home</span>
       </Link>
-      <button className='bg-blue-400 text-white rounded-full
-       hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline'>
-        Sign In
-      </button>
+      {session ? (
+        <button
+          className="bg-blue-400 text-white rounded-full
+         hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
+          onClick={() => signOut()}
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          className="bg-blue-400 text-white rounded-full
+       hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
+          onClick={() => signIn()}
+        >
+          Sign In
+        </button>
+      )}
     </div>
-  ); 
+  );
 }
